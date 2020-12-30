8287a944d93cbcd1798180a8f3bad7d1c7159d76
osha7 <ogroetz@gmail.com>
2020-12-30 14:35:31 -0700

    user created password and bycrypt set up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+
+    def index
+        users = User.all
+        render json: {
+            users: users
+        }
+    end
+
+    def create
+        user = User.create(user_params)
+        if @user
+            render json: {
+                status: :created
+                user: user
+            }
+        else
+            render json: {
+                status: 500
+            }
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password, :password_digest)
+    end
+
 end
